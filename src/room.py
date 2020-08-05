63ec0a77d6ad0eef6d821f09f9be37860165d411
imdeja <61983284+imdeja@users.noreply.github.com>
2020-08-05 10:25:46 -0700

    Update room.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description, items=None):
+        self.name = name
+        self.description = description
+        if items is None:
+            self.items = {}
+        else:
+            self.items = {item.id: item for item in items}
+
+        # Room direction
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    # Return values from item dictionary 
+    def view_items(self):
+        return items.items()
